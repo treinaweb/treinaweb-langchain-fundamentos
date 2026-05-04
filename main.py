6073c3b072157c7be1b814dad4bc17294a7ccb7a
Cleyson Lima <cleysonph@gmail.com>
2026-05-04 16:58:18 -0300

    02:04 - Trabalhando com tools

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 from langchain.chat_models import init_chat_model
-from langchain.messages import SystemMessage, HumanMessage
+from langchain.messages import HumanMessage
+from langchain.tools import tool
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
+@tool
+def calculate_bmi(weight: float, height: float) -> float:
+    """
+    Calculate Body Mass Index (BMI).
+
+    Args:
+        weight: Body weight in kilograms.
+        height: Body height in meters.
+
+    Returns:
+        BMI value rounded to 2 decimal places.
+    """
+    bmi = weight / (height**2)
+    return round(bmi, 2)
+
+
 model = init_chat_model(model="gpt-4.1-mini", temperature=0.7)
+models_with_tools = model.bind_tools([calculate_bmi])
 
 messages = [
-    SystemMessage(
-        content="Você é um assistente que responde apenas em forma de haiku."
-    ),
-    HumanMessage(content="Como funciona o Langchain Framework?"),
+    HumanMessage(content="Qual o IMC de alguém com 70Kg e 1.75m?"),
 ]
 
-for chunk in model.stream(messages):
-    print(chunk.content, end="", flush=True)
+response = models_with_tools.invoke(messages)
+messages.append(response)
+
+for tool_call in response.tool_calls:
+    result = calculate_bmi.invoke(tool_call)
+    messages.append(result)
+
+final = models_with_tools.invoke(messages)
+print(final)
