06669ddad77b589e634bf546ae0c0f15ed21637b
Cleyson Lima <cleysonph@gmail.com>
2026-05-04 16:18:56 -0300

    02:03 - Tipos de mensagens e stream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from langchain.chat_models import init_chat_model
+from langchain.messages import SystemMessage, HumanMessage
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,6 +7,12 @@ load_dotenv()
 
 model = init_chat_model(model="gpt-4.1-mini", temperature=0.7)
 
-response = model.invoke("O que é o Langchain Framework?")
+messages = [
+    SystemMessage(
+        content="Você é um assistente que responde apenas em forma de haiku."
+    ),
+    HumanMessage(content="Como funciona o Langchain Framework?"),
+]
 
-print(response.content)
+for chunk in model.stream(messages):
+    print(chunk.content, end="", flush=True)
